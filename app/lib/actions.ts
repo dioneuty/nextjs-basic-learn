3114025f1a087ef8f469b139ff7d540659677714
dioneuty <1@293f3900>
2024-07-18 16:43:58 +0900

    Chapter 14. Improving Accessibility

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,27 +7,53 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
-    id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
-    date: z.string(),
-})
+  id: z.string(),
+  customerId: z.string({
+    invalid_type_error: '고객을 선택하세요.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: '0달러보다 높은 금액을 입력하세요.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: '송장 상태를 선택하세요.',
+  }),
+  date: z.string(),
+});
 
 const CreateInvoice = FormSchema.omit({id: true, date: true});
 
-export async function createInvoice(formData: FormData) {
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
 //   const rawFormData = {
 //     customerId: formData.get('customeId'),
 //     amount: formData.get('amount'),
 //     status: formData.get('status'),
 //   }
 
-    const { customerId, amount, status } = CreateInvoice.parse({
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     })
+
+    // 만약 폼 유효성 검사에 실패하면, 즉시 에러를 반환합니다. 그렇지 않으면 계속 진행합니다.
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: '필수 항목이 없습니다. 송장을 생성하지 못했습니다.',
+      };
+    }
+
+      // 데이터베이스에 삽입할 데이터 준비
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
@@ -49,13 +75,21 @@ export async function createInvoice(formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+    const validatedFields = UpdateInvoice.safeParse({
       customerId: formData.get('customerId'),
       amount: formData.get('amount'),
       status: formData.get('status'),
     });
+
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Update Invoice.',
+      };
+    }
   
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
   
     try {
