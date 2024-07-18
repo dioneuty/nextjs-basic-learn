e15a7408551fab02bcb69f171b8273b7a523e890
dioneuty <1@293f3900>
2024-07-18 15:58:05 +0900

    Chapter 13. Handling Errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -34,12 +34,17 @@ export async function createInvoice(formData: FormData) {
   const rawFormData = Object.fromEntries(formData.entries())
   // 테스트해보세요:
 //   console.log(typeof rawFormData.amount);
-
-  await sql`  INSERT INTO invoices (customer_id, amount, status, date)
+  try{
+    await sql`  INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+  } catch (error) {
+    return {
+      message: '데이터베이스 오류: 인보이스 생성에 오류가 발생하였습니다.'
+    };
+  }
 
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
@@ -53,15 +58,24 @@ export async function updateInvoice(id: string, formData: FormData) {
   
     const amountInCents = amount * 100;
   
-    await sql`  UPDATE invoices
+    try {
+      await sql`  UPDATE invoices
       SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
       WHERE id = ${id}`;
-  
+    } catch (error) {
+      return {message: '데이터베이스 오류: 인보이스 수정에 오류가 발생하였습니다.'}
+    }
+
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
+  try{
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
+    return { message: '인보이스가 삭제되었습니다'}
+  } catch(error){
+    return { message: '데이터베이스 오류: 인보이스 삭제에 오류가 발생하였습니다'}
+  }
 }  
